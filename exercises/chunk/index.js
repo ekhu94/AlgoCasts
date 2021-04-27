43ecc0d67650d0e2fe188435f824d35d70cc1768
Erik Huang <ekhu94@gmail.com>
2021-04-27 10:26:31 -0700

    reviewed using slice

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,14 +10,10 @@
 
 function chunk(array, size) {
   const output = [];
-  for (let el of array) {
-    // if the output is empty OR if the length of last element in the output array is equal to size, create a new chunk
-    if (!output.length || output[output.length - 1].length === size) {
-      output.push([el]);
-      // else, push the current element into the last chunk of the output
-    } else {
-      output[output.length - 1].push(el);
-    }
+  let i = 0;
+  while (i < array.length) {
+    output.push(array.slice(i, i + size));
+    i += size;
   }
   return output;
 }
