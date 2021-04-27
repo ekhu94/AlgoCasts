38d48b50b160f0b4200e99aac915623e6109d82c
Erik Huang <ekhu94@gmail.com>
2021-04-27 09:43:02 -0700

    maxchars done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,16 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const counter = {};
+  for (let char of str) {
+    counter[char] = counter[char] + 1 || 1;
+  }
+  return Object.keys(counter).reduce((max, val) =>
+    counter[max] > counter[val] ? max : val
+  );
+}
+
+maxChar("fuckerrrrr");
 
 module.exports = maxChar;
