7e0d8ccf924d9c42ab908408bfcd7741214f68c4
Erik Huang <ekhu94@gmail.com>
2021-04-28 11:32:20 -0700

    complete capitalize function with string output

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,16 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let output = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      output += str[i].toUpperCase();
+    } else {
+      output += str[i];
+    }
+  }
+  return output;
+}
 
 module.exports = capitalize;
