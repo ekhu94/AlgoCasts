71f2fbb60781ca04208a23d4ad86d6c2f6bc13ba
Erik Huang <ekhu94@gmail.com>
2021-05-07 12:26:09 -0700

    weave finished

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,17 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+  const q = new Queue();
+  while (sourceOne.data.length || sourceTwo.data.length) {
+    if (sourceOne.peek()) {
+      q.add(sourceOne.remove());
+    }
+    if (sourceTwo.peek()) {
+      q.add(sourceTwo.remove());
+    }
+  }
+  return q;
+}
 
 module.exports = weave;
